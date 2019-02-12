7de4bf3f2217e55d36999c5e4a98ed452f76eeda
Jon <jonpoischen@gmail.com>
2019-02-12 14:28:12 +0100

    Jon testing if he can commit

--- jobdirecto.sql
+++ jobdirecto.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS jobs;
 
-CREATE TABLE jobs3(
+CREATE TABLE jobs(
     id SERIAL PRIMARY KEY,
     restName VARCHAR(300),
     jobType VARCHAR(255),
@@ -23,6 +23,6 @@ New York, NY 10281', '(212) 981-8577', 'Manhattan');
 INSERT INTO jobs (restName, jobType, hourPay, typePay, schedule, contact, address, phone, area) VALUES ('Daniel', 'Lavaplatos', '15', 'Cash', 'lunes, martes, miercoles sabados y domingos', 'juan', '60 E 65th St.
 New York, NY 10065', '(212) 288-0033', 'Manhattan');
 INSERT INTO jobs (restName, jobType, hourPay, typePay, schedule, contact, address, phone, area) VALUES ('Ambrosio Italian Restaurant & Banquet Hall', 'Lavaplatos', '13', 'ambos', 'fines de semana', 'maria', '2071 Clove Rd
-Staten Island, NY 10304', '(718) 524-7174', 'Staten Island');;
+Staten Island, NY 10304', '(718) 524-7174', 'Staten Island');
 INSERT INTO jobs (restName, jobType, hourPay, typePay, schedule, contact, address, phone, area) VALUES ('L’Appart', 'Cocinero', '16', 'Check', 'lunes marte y miercoles', 'nicola', '225 Liberty St
 New York, NY 10281', '(212) 981-8577', 'Manhattan');
