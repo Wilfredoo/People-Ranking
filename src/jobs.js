8e1c8f0def6dae4578eb6e474b578b5fea094000
wilfredo <casas.farach@gmail.com>
2019-02-16 00:35:54 +0100

    not broken

--- src/jobs.js
+++ src/jobs.js
@@ -15,16 +15,9 @@ export class Jobs extends React.Component {
     componentDidMount() {
       console.log("look at me");
         axios.get("/getJobs").then(result => {
-            // for (var i = 0; i < result.data.data.length; i++) {
-            //     if(result.data.data[i].jobtype !== "Otro") {
-            //         result.data.data[i].otro_job = false;
+
                     this.setState({jobData: result.data});
-            //     } else if (result.data.data[i].jobtype === "Otro") {
-            //         result.data.data[i].jobtype = result.data.data[i].otro_desc;
-            //         result.data.data[i].otro_job = true;
-            //         this.setState({jobData: result.data});
-            //     }
-            // }
+
         });
 
 
@@ -32,15 +25,11 @@ export class Jobs extends React.Component {
 
 
 
-    // <h3>{data.restname}
-    //     <span className="busca"> busca </span>
-    //     {data.jobtype}</h3>
-
     render() {
 
         return (
             <div className="bg">
-                <h1>JobDirecto - Trabajos en NYC</h1>
+                <h1>PuaRanking - A modest list</h1>
 
             </div>
         );
